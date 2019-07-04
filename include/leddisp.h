effe26a7afa36325c4f4cdf024fa3d56cd78907a
aerccu <aerccu@gmail.com>
2019-07-04 10:00:41 +0200

    basic function prototypes finished

--- include/leddisp.h
+++ include/leddisp.h
@@ -69,38 +69,137 @@ namespace display{
                 int parallel_disps = 1);
 
         virtual ~RGBMatrix();
+
+
+        void setGPIO(GPIO *io, bool initThread = true);
+
+        void startRefresh();
+
+        void ApplyPmapper(const Pmapper *pmapper);
+
+        bool setPWMBits(uint8_t val);
+        uint8_t getPWMBits();
+
+        void setAdjustedLuminance(bool on);
+        bool getAdjustedLuminance();
+
+        void setBrightness(uint8_t val);
+        uint8_t getBrightness();
+
+        GPIO* getGPIO() { return io_; }
+
+        uint32_t onTimedInputChange(int timeout_ms);
+
+        // frameCanvas : canvas
+        FrameCanvas* createFrameCanvas();
+
+        FrameCanvas* swapOnVSync(FrameCanvas* fcan, unsigned framerate_fraction = 1);
+
+
+        // interface
+        virtual int width() const;
+        virtual int height() const;
+        virtual void fill(uint8_t r, uint8_t g, uint8_t b);
+        virtual void setPix(int x, int y, uint8_t r, uint8_t g, uint8_t b);
+        virtual void clear();
+
+        
         
 
+        private:
+        class UpdateThread;
+        friend class UpdateThread;
 
+        void applyNamedPmappers(const char* pmapper_conf, int chain, int parallel);
 
+        Options params_;
+        bool luminance_correction_;
 
+        FrameCanvas* active_;
 
+        GPIO* *io_;
+        Mutex active_frame_sync;
 
-
-
+        UpdateThread* update_;
+        std::vector<FrameCanvas*> created_frames_;
+        internal::PixelDesignatorMap* shared_pmapper_;
     };
 
 
 
+    class FrameCanvas : public Canvas {
 
+        public:
 
+        // true on success
+        bool setPWMBits(uint8_t val);
+        uint8_t pwmbits();
 
+        
+        void setLumixCorrect(bool on);
+        bool getLumixCorrect() const;
 
+        void setBrightness(uint8_t brightness);
+        uint8_t getBrightness();
 
+        // storage & loading (true on success)
+        void serialise(const char** data, size_t* len) const;
+        bool deserialise(const char *data, size_t len);
 
+        void copyFrom(const FrameCanvas &other);
 
+        // interface
+        virtual int width() const;
+        virtual int height() const;
+        virtual void setPix(int x, int y,
+                            uint8_t r,
+                            uint8_t g,
+                            uint8_t b);
 
+        virtual void fill(uint8_t r,
+                          uint8_t g,
+                          uint8_t b);
 
+        virtual void clear();
 
+        private:
+        friend class RGBMatrix;
 
+        FrameCanvas(internal::FrameBuffer* frame) : frame_(frame) {}
+        virtual ~FrameCanvas();
+        internal::FrameBuffer* framebuffer() { return frame_; }
+
+        internal::FrameBuffer* const frame_;
+    };
+
+    struct RuntimeOptions {
+        RuntimeOptions();
+
+        int gpio_slowdown;
+        int daemon;
+        int invalidate_privileges;
+        bool do_gpio_init;
+    };
+
+    // parse and update Options Struct
+    bool parseOptions(int* argc, char*** argv, 
+                        RGBMatrix::Options* default_options,
+                        RuntimeOptions* rt_op,
+                        bool remove_processed_flags = true);
+
+    // initialising matrix from options
+    RGBMatrix* createMatrixFromOptions(const RGBMatrix::Options &options,
+                                        const RuntimeOptions &rt_options);
+
+    // matrix creation from parsed flags and from passed flags
+    RGBMatrix* createMatrix(int* argc, char*** argv,
+                            RGBMatrix::Options* default_ops = NULL,
+                            RuntimeOptions* default_rt_opts = NULL,
+                            bool remove_processed_flags = true);
+
+    // display options
+    void printMatrixOptions(FILE* out, const RGBMatrix::Options* default_ops = NULL,
+                            RuntimeOptions* default_rt_ops = NULL,
+                            bool remove_processed_flags = true);
 }
-
-
-
-
-
-
-
-
-
 #endif // LED_DISP
