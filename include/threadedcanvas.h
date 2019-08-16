9b437a5faf1b11c46d6cdc3de9191709d63a39f2
aerccu <aerccu@gmail.com>
2019-08-16 08:26:11 +0200

    adding class skeleton

---
+++ include/threadedcanvas.h
@@ -0,0 +1,35 @@
+
+
+
+#ifndef THREADEDCANVAS_H
+#define THREADEDCANVAS_H
+
+#include "thread.h"
+#include "canvas.h"
+
+namespace display {
+
+    class ThreadedCanvas : public Thread {
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+#endif // THEADEDCANVAS_H
